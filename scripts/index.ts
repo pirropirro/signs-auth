e9bfddded1d5fe147525f4b2afc1cd27e5d8b248
pirropirro <fra.pirrotta@gmail.com>
2019-03-13 18:02:17 +0100

    Remove useless export

--- scripts/index.ts
+++ scripts/index.ts
@@ -2,7 +2,6 @@ export { Tokens } from "./auth/Tokens";
 export { AuthModule } from "./auth/AuthModule";
 export { IAuthConfig } from "./auth/IAuthConfig";
 export { IAuthClient } from "./auth/IAuthClient";
-export { AuthDecorator } from "./auth/AuthDecorator";
 export { Authenticate, AuthenticateAll } from "./auth/Authenticate";
 
 export { IJWTService } from "./auth/jwt/IJWTService";
