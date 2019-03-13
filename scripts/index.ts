3a97ebff93b15b2159405113ee2fc879378d4406
pirropirro <fra.pirrotta@gmail.com>
2019-03-13 18:02:01 +0100

    Export services

---
+++ scripts/index.ts
@@ -0,0 +1,11 @@
+export { Tokens } from "./auth/Tokens";
+export { AuthModule } from "./auth/AuthModule";
+export { IAuthConfig } from "./auth/IAuthConfig";
+export { IAuthClient } from "./auth/IAuthClient";
+export { AuthDecorator } from "./auth/AuthDecorator";
+export { Authenticate, AuthenticateAll } from "./auth/Authenticate";
+
+export { IJWTService } from "./auth/jwt/IJWTService";
+export { IRSAKeyRetriever } from "./auth/jwt/IRSAKeyRetriever";
+
+export { ITokenService } from "./auth/token/ITokenService";
